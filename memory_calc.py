3b49f6ad424d48bd5752813183f089b00e863dd6
zeekus <31280061+zeekus@users.noreply.github.com>
2018-07-07 09:34:53 -0400

    Update memory_calc.py

--- memory_calc.py
+++ memory_calc.py
@@ -19,15 +19,17 @@ for line in running_procs:
   array_of_ps_output=[]
   array_of_ps_output=line.split() 
   matchObj = re.search(r'\d',array_of_ps_output[5]) #look for digits on 5th column of output Resident memory
-  matchObj1 = re.search(r'\d',array_of_ps_output[4]) #look for digits on 4th column of output Virtual memory
+  matchObj1 = re.search(r'\d',array_of_ps_output[4]) #look for digits on 4th column of output VSV
    
   if matchObj:
    resident_mem=array_of_ps_output[5]
    total_resident_memory_used = ( total_resident_memory_used + int(resident_mem)) 
 
-  if matchObj1:
-   vmem=array_of_ps_output[4]
-   total_virt_memory_used = ( total_virt_memory_used + int(vmem)) 
+  #if matchObj1:
+  # vmem=array_of_ps_output[4]
+  # total_virt_memory_used = ( total_virt_memory_used + int(vmem)) 
      
 print "total resident memory used by applications from process tree: " + str(total_resident_memory_used/1024) + " Mb"
-print "total virtual memory used by applications from process tree: " + str(total_virt_memory_used/1024) + " Mb"
+
+#VSV is the total virtual memory a process has available so this is not correct
+#print "total virtual memory used by applications from process tree: " + str(total_virt_memory_used/1024) + " Mb"
