5b583e292423d508228e68a64f98ac8f96c57f83
zeekus <ipmasqman@gmail.com>
2020-12-26 00:45:28 -0500

    debugged and tested

--- ksp/simple_kerbal_lancher.py
+++ ksp/simple_kerbal_lancher.py
@@ -24,7 +24,7 @@ To run this KSP kRPC Python program you need:
 - Python 3.8 (https://www.python.org/download/releases/3.8/)
 """
 import krpc
-import time
+from time import localtime, strftime, sleep
 import math # for orbital math
 from sys import exit #for exit routine
 
@@ -63,14 +63,15 @@ text4.color = (1, 2, 1)
 text4.size = 20
 rect.position=(-screen_x,300 )
 
-time.sleep(2)
+sleep(2)
 
 
 # Some values for altitudes
-turn_start_altitude = 250
-turn_end_altitude = 45000
+turn_start_altitude = 5000
+turn_end_altitude = 40000
 target_altitude=80000 #target_apoapsis
 target_periapsis=75000
+gravity_turn_limit=35 # don't go greater than 35 degrees
 
 # Now we're actually starting
 vessel = conn.space_center.active_vessel
@@ -84,65 +85,88 @@ srf_speed = conn.add_stream(getattr, vessel.flight(srf_frame), 'speed')
 dynamic_pressure = conn.add_stream(getattr, vessel.flight(srf_frame), 'dynamic_pressure')
 gforce = conn.add_stream(getattr, vessel.flight(srf_frame), 'g_force')
 liquidfuel=vessel.resources.amount('LiquidFuel') #initial_solidfuel
-#lqfstream = conn.add_stream(getattr, vessel.flight(srf_frame), 'LiquidFuel')
 solidfuel=vessel.resources.amount('SolidFuel') #initial_liquidfuel
 aero=conn.add_stream(getattr, vessel.flight(srf_frame), 'aerodynamic_force')
 vs=conn.add_stream(getattr, vessel.flight(srf_frame), 'vertical_speed')
 adensity=conn.add_stream(getattr, vessel.flight(srf_frame), 'atmosphere_density')
+longitude=conn.add_stream(getattr, vessel.flight(), 'longitude')
+latitude=conn.add_stream(getattr, vessel.flight(), 'latitude')
 
-
-stage=0
-
-#functions
-def print_telemetry(solidfuel,liquidfuel):
-   print('atmosphere_density: {0:1.0f}'.format(adensity()))
-   print('Altiude           : {0:1.0f}'.format(altitude()))
-   print('vertical_speed    : {0:1.0f} m/s'.format(vs()) )
-   if dynamic_pressure() > 10000 :
-     print('Speed             : {0:1.0f} m/s *TOO FAST*'.format(math.trunc(srf_speed())) )
-     print('Dynamic Pressure  : {0:1.0f} psi'.format(math.trunc(dynamic_pressure())) )
+def print_telemetry(solidfuel,liquidfuel,lfb_active,slb_active):
+   mytime=(strftime("%d %b %Y %H:%M:%S",localtime())) #timestamp
+   print('{1} atmosphere_density: {0:1.0f}'.format(adensity(),mytime))
+   print('{1} Altitude          : {0:1.0f}'.format(altitude(),mytime))
+   print('{1} vertical_speed    : {0:1.0f} m/s'.format(vs(),mytime) )
+   if dynamic_pressure() > 15000 :
+     print('{1} Speed             : {0:1.0f} m/s *TOO FAST*'.format(math.trunc(srf_speed()),mytime) )
+     print('{1} Dynamic Pressure  : {0:1.0f} psi'.format(math.trunc(dynamic_pressure()), mytime) )
    else:
-     print('Speed             : {0:1.0f} m/s'.format(math.trunc(srf_speed())) )
-     print('Dynamic Pressure  : {0:1.0f} psi'.format(math.trunc(dynamic_pressure())) )
-   print('mean_altitude     : {0:1.0f}'.format(altitude()))
-   print('apoapsis          : {0:1.0f}'.format(apoapsis()))
+     print('{1} Speed             : {0:1.0f} m/s'.format(math.trunc(srf_speed()), mytime) )
+     print('{1} Dynamic Pressure  : {0:1.0f} psi'.format(math.trunc(dynamic_pressure()),mytime) )
+   print('{1} mean_altitude     : {0:1.0f}'.format(altitude(),mytime))
+   print('{1} apoapsis          : {0:1.0f}'.format(apoapsis(),mytime))
    #passing local variables to next function
-   remaining_fuel(initial_solidfuel=solidfuel,initial_liquidfuel=liquidfuel)
+   remaining_fuel(lfb_active,slb_active,mytime, initial_solidfuel=solidfuel,initial_liquidfuel=liquidfuel)
    #print('universal time to warp is %s' % ut() )
 
-def remaining_fuel(initial_solidfuel,initial_liquidfuel):
-
-   if vessel.resources.amount('SolidFuel') > 0.1:
-    sfuel =  vessel.resources.amount('SolidFuel')
-    print ('solid fuel        : {0:1.0f}% or {1:1.0f}'.format((sfuel/initial_solidfuel)*100,sfuel)) #percent remaining
-    print ('solid fuel used   : {0:1.0f} %'.format(((initial_solidfuel - sfuel) /initial_solidfuel) * 100)) #percent used
-   else:
-    lfuel =  vessel.resources.amount('LiquidFuel')
-    print ('liquid fuel       : {0:1.0f}% or {1:1.0f}'.format((lfuel/initial_liquidfuel)*100,lfuel)) #precent remaining
-    print ('liquid fuel used  : {0:1.0f}%'.format(((initial_liquidfuel - lfuel) /initial_liquidfuel) * 100)) #precent used
-   if altitude() > 50000 and vessel.resources.amount('LiquidFuel') < 0.1:
+def remaining_fuel(lfb_active,slb_active,mytime, initial_solidfuel,initial_liquidfuel):
+   if solid_fuel_level() > 0.1 and slb_active is True:
+     print ('{2} solid fuel        : {0:1.0f}% or {1:1.0f}'.format((solid_fuel_level()/initial_solidfuel)*100,solid_fuel_level(),mytime)) #percent remaining
+     print ('{1} solid fuel used   : {0:1.0f} %'.format(((initial_solidfuel - solid_fuel_level() ) /initial_solidfuel) * 100,mytime)) #percent used
+   if liq_fuel_level() > 0.1 and lfb_active is True:
+      print ('{2} liquid fuel       : {0:1.0f}% or {1:1.0f}'.format((liq_fuel_level()/initial_liquidfuel)*100,liq_fuel_level(),mytime)) #precent remaining
+      print ('{1} liquid fuel used  : {0:1.0f}%'.format(((initial_liquidfuel - liq_fuel_level() ) /initial_liquidfuel) * 100,mytime)) #precent used
+   if altitude() > 1000 and solid_fuel_level() < 0.1 and liq_fuel_level() < 0.1:
       exit("We ran out of fuel. :'( Exiting")
 
+def solid_fuel_level():
+    solidfuel_level=round(vessel.resources.amount('SolidFuel'),2) #get solid fuel level
+    return solidfuel_level
 
-def dynamic_throttle_control():
-     if dynamic_pressure()/9000 >1 and vs() > 100:
-      #too fast wasting fuel during ascent
-      x = 0.5 - (dynamic_pressure()/9000)/5
-     elif dynamic_pressure()/4000 < 0.8:
-      x = 1
-     elif dynamic_pressure()/9000 > 1:
-      x = .5
+def liq_fuel_level():
+    liquidfuel_level=round(vessel.resources.amount('LiquidFuel'),2) #get liquidfuel level
+    return liquidfuel_level
+
+def basic_telemetry():
+     #get basic telmetry from one second ago and pass it back to main
+     #1sp = 1 second in past
+     sfb_1sp = solid_fuel_level()
+     lfb_1sp = liq_fuel_level()
+     vertical_speed_1sp = vs()
+     altitude_1sp = altitude()
+     latitude_1sp=latitude()
+     longitude_1sp=longitude()
+     ut_1sp=ut()
+     sleep(1)
+     if sfb_1sp == solid_fuel_level():
+       sfb_active=False
      else:
-      x = 1
-     #engine burnout protection
-     if x == 0 or x < .11: #zero breaks this program burnouts at .10
-        x=0.11
-     vessel.control.throttle = x
-     print ("throttle: %s" % x )
-     if x < 1:
-         time.sleep(2)
-     return x # we need to know the thottle settings
+       sfb_active=True
+     if lfb_1sp == liq_fuel_level():
+       lfb_active=False
+     else:
+       lfb_active=True
 
+     return (lfb_active,sfb_active,sfb_1sp,lfb_1sp,vertical_speed_1sp,altitude_1sp,latitude_1sp,longitude_1sp,ut_1sp)
+
+
+def dynamic_throttle_control(override):
+  if dynamic_pressure()/19000 >1 and vs() > 5000:
+      #too fast wasting fuel during ascent
+      x = 0.5
+  elif dynamic_pressure()/4000 < 0.8 or altitude() < 15000:
+      #low resistance or too altitude run at full power
+      #gravity sucks
+      x = 1
+  elif dynamic_pressure()/10000 > 1:
+     #moderate atmospheric resistance
+     x = .8
+  elif override>0:
+     x = override
+  else:
+     x =.75
+
+  return x # we need to know the thottle settings
 
 vessel.auto_pilot.engage() #enable auto_pilot
 
@@ -150,7 +174,7 @@ vessel.auto_pilot.engage() #enable auto_pilot
 for i in range ( 3, 0 , -1):
   text1.content = ('%s...' % i)
   print('%s...' % i)
-  time.sleep(1)
+  sleep(1)
 
 # Set Pitch = Heading 90/90 is stright up
 text1.content = 'Autopilot enabled'
@@ -159,7 +183,7 @@ text3.content = 'Pitch 90/90'
 text4.content = 'Stage 0'
 
 print('Set pitch straight up. 90/90')
-time.sleep(1)
+sleep(1)
 #90,90 is stright up
 #90,45 is west
 #90,270 is east
@@ -172,7 +196,7 @@ mystage=0 # tracking the stage
 
 
 #solid booster check
-if vessel.resources.amount('SolidFuel') > 0.1:
+if solid_fuel_level() > 0.1:
   solid_booster_attached=True # we assume one is not attached
   solid_booster_seperated=False
   text3.content = 'SolidFuel on'
@@ -191,22 +215,26 @@ turn_angle=90 #no turn on start
 #ascent loop start
 while True:
 
+    (lfb_active,sfb_active,sfb_1sp,lfb_1sp,vs_1sp,alt_1sp,lat_1sp,long_1sp,ut_1sp)=basic_telemetry()
+
     ####
     ##booster type detection and burn
     ####
     #solid booster attached and with fuel
-    if vessel.resources.amount('SolidFuel') > 0.1 and solid_booster_attached is True:
+    if solid_fuel_level() > 0.1 and sfb_active is True:
       text3.content = 'SolidFuel burning' #launches solid fuel
       text4.content = 'SB Stage {}'.format(mystage)
       print('Status            : solid booster active')
-      time.sleep(1)
+      throttle=dynamic_throttle_control(0)
+      vessel.control.throttle = throttle
+      sleep(1)
     #solid booster attached but fuel is spent
-    elif vessel.resources.amount('SolidFuel') < 0.1 and solid_booster_attached is True:
+    elif solid_fuel_level() < 0.1 and solid_booster_attached is True:
       text3.content = 'SolidFuel spent' #launches solid fuel
       vessel.control.activate_next_stage()
-      time.sleep(1)
+      sleep(1)
       text3.content = 'SolidFuel dropped' #launches solid fuel
-      time.sleep(1)
+      sleep(1)
       mystage=mystage+1
       text4.content = 'LB Stage {}'.format(mystage)
       solid_booster_attached=False
@@ -214,53 +242,66 @@ while True:
 
 
     #solid booster absent and liquid fuel is greater than zero
-    elif solid_booster_attached is False and vessel.resources.amount('LiquidFuel') > 0.1:
+    elif solid_booster_attached is False and liq_fuel_level() > 0.1 and altitude() < turn_end_altitude:
+      (lfb_active,sfb_active,sfb_1sp,lfb_1sp,vs_1sp,alt_1sp,lat_1sp,long_1sp,ut_1sp)=basic_telemetry()
       #monitor fuel
       text3.content = 'LiquidFuel burn'
       print('Status            : Liquid Burn Activated')
-      liquidfuel_level_before_wait=vessel.resources.amount('LiquidFuel')
-      throttle=dynamic_throttle_control()
-      time.sleep(1)
-      liquidfuel_level_after_wait=vessel.resources.amount('LiquidFuel')
+      throttle=dynamic_throttle_control(0)
+      vessel.control.throttle = throttle
 
-      if liquidfuel_level_before_wait == liquidfuel_level_after_wait:
+      if liq_fuel_level() == lfb_1sp :
          text3.content = 'Out of Fuel'
          vessel.control.activate_next_stage() #out of fuel change stage
-         time.sleep(1)
+         sleep(1)
          mystage=mystage+1
          print('Status            : Out of Fuel')
          text4.content = 'LB Stage {}'.format(mystage)
     #no fuel
+    elif solid_booster_attached is False and liq_fuel_level() > 0.1 and altitude() > turn_end_altitude:
+        #get basic telemetry
+        (lfb_active,sfb_active,sfb_1sp,lfb_1sp,vs_1sp,alt_1sp,lat_1sp,long_1sp,ut_1sp)=basic_telemetry()
+        #moving to gravity turn logic
+
     else:
       text1.content = 'problem detected.'
       text2.content = 'exiting.'
       text3.content = 'out of fuel'
       text4.content = 'Stage {}'.format(mystage)
-      time.sleep(5)
+      sleep(5)
       print('Status            : out of fuel ')
       exit("out of fuel")
 
 
-    #print telemetry
-    print_telemetry(solidfuel,liquidfuel)
+    # print telemetry
+    print_telemetry(solidfuel,liquidfuel,lfb_active,sfb_active)
+
 
     # Gravity turn
     if altitude() > turn_start_altitude and altitude() < turn_end_altitude:
       frac = ((altitude() - turn_start_altitude) /
                 (turn_end_altitude - turn_start_altitude))
       new_turn_angle = frac * 90
-      if abs(new_turn_angle - turn_angle) > 0.5:
-        turn_angle = new_turn_angle
-        print('Status            : gravity turn with {0}'.format(turn_angle))
-        text1.content = "Gravity Turn started"
-        text2.content = "turn is {}".format(90-turn_angle)
-        vessel.auto_pilot.target_pitch_and_heading(90-turn_angle, 90)
+      if abs(new_turn_angle - turn_angle) > 1:
+        turn_angle = round(new_turn_angle)
+
+        if (90-turn_angle) > gravity_turn_limit:
+          vessel.auto_pilot.target_pitch_and_heading(90-turn_angle, 90)
+          print('Status            : gravity turn angle is {0}'.format(turn_angle))
+          text1.content = "Gravity Turn deeper"
+          text2.content = "turn is {}".format(90-turn_angle)
+        else:
+          print('Status            : gravity turn angle limit reached.')
+          text2.content = "turn limit reached"
+
 
     # Decrease throttle when approaching target apoapsis
-    if apoapsis() > target_altitude*0.80:
+    if apoapsis() > target_altitude*0.80 and apoapsis() < target_altitude*.95:
       text1.content = 'nearing TA 80%'
       text2.content = 'throttle 25%'
-      vessel.control.throttle = .25
+      throttle=dynamic_throttle_control(.25)
+      vessel.control.throttle = throttle
+      sleep(1)
       print('Approaching target apoapsis.')
     elif apoapsis() > target_altitude*0.95:
       text1.content = 'nearing TA 95%'
@@ -273,12 +314,12 @@ while True:
 text1.content = 'Engine off'
 text2.content = 'coasting to AP'
 vessel.control.throttle = 0
-time.sleep(1)
+sleep(1)
 
 
 # Wait until we're on 90% of the apoapsis.
 while vessel.flight().mean_altitude < target_altitude*0.90:
-   time.sleep(.2)
+   sleep(.2)
    text3.content = 'waiting {}'.format(target_altitude-altitude())
 
 text1.content = 'Starting turn'
@@ -322,32 +363,27 @@ conn.space_center.warp_to(burn_ut - lead_time)
 print('Ready to execute burn')
 time_to_apoapsis = conn.add_stream(getattr, vessel.orbit, 'time_to_apoapsis')
 while time_to_apoapsis() - (burn_time/2.) > 0:
-    print_telemetry(solidfuel,liquidfuel)
-    #text.content = 'Dynamic Pressure: %d psi' % (vessel.thrust/1000)
-    #text2.content = 'Speed: %d m/s' % (vessel.flight(srf_frame).speed)
-    #text3.content = 'Altitude: %d m' % (vessel.flight(srf_frame).surface_altitude)
-    time.sleep(0.1)
+    sleep(0.1)
 print('Executing burn')
 vessel.control.throttle = 1.0
-time.sleep(burn_time - 0.5)
+sleep(burn_time - 0.5)
 print('Fine tuning')
 vessel.control.throttle = 0.05
 remaining_burn = conn.add_stream(node.remaining_burn_vector, node.reference_frame)
 while remaining_burn()[1] > 1:
-    print (remaining_burn())
-    print_telemetry(solidfuel,liquidfuel)
-    text4.content=(remaining_burn())
-    #text.content = 'Dynamic Pressure: %d psi' % (vessel.thrust/1000)
-    #text2.content = 'Speed: %d m/s' % (vessel.flight(srf_frame).speed)
-    #text3.content = 'Altitude: %d m' % (vessel.flight(srf_frame).surface_altitude)
-    time.sleep(0.1)
+    sleep(0.1)
 vessel.control.throttle = 0.0
 node.remove()
 
 text1.content = 'Success'
 text2.content = 'Orbital burn complete'
 
-print_telemetry(solidfuel,liquidfuel)
+#get basic telemetry
+(lfb_active,sfb_active,sfb_1sp,lfb_1sp,vs_1sp,alt_1sp,lat_1sp,long_1sp,ut_1sp)=basic_telemetry()
+
+# print telemetry
+print_telemetry(solidfuel,liquidfuel,lfb_active,sfb_active)
+
 
 #
 # text.content = 'Extend solar panels'
