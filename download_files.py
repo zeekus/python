aee806502e922b6e53f56e8391e590d18e0717dc
Theodore Knab <ipmasqman@gmail.com>
2021-05-17 07:44:30 -0400

    edit

---
+++ download_files.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.3
+#filename: download_files.py
+#description: download files from a list
+#source: https://stackoverflow.com/questions/3173372/download-files-from-a-list-if-not-already-downloaded
+import os.path
+import urllib.request
+
+links = open('links.txt', 'r')
+for link in links:
+    link = link.strip()
+    name = link.rsplit('/', 1)[-1]
+    filename = os.path.join('downloads', name)
+
+    if not os.path.isfile(filename):
+        print('Downloading: ' + filename)
+        try:
+            urllib.request.urlretrieve(link, filename)
+        except Exception as inst:
+            print(inst)
+            print('  Encountered unknown error. Continuing.')
