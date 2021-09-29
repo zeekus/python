0ccf652136e360c1da6850ad2eab5f3a22c9d041
zeekus <ipmasqman@gmail.com>
2021-09-29 19:39:45 -0400

    updates

---
+++ selenium/quickstart_selenium.md
@@ -0,0 +1,10 @@
+# quick start : Selenium
+
+```
+  pip3 install bs4
+  pip3 install request
+  pip3 install requests
+  pip3 install chromedriver
+  sudo pacman -Syuu chromium
+```
+
--- selenium/read_text_annapolislinux.py
+++ selenium/read_text_annapolislinux.py
@@ -4,22 +4,42 @@
 #headless chrome example
 #requirements chromedriver
 
+#tested 29 Sept 29 2021
+
 from bs4 import BeautifulSoup
-import requests
-import html5lib
+#import requests
+#import html5lib
 import lxml
 from selenium import webdriver
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--incognito')
-options.add_argument('--headless')
+myoptions = webdriver.ChromeOptions()
+myoptions.add_argument('--ignore-certificate-errors')
+myoptions.add_argument('--incognito')
+myoptions.add_argument('--headless')
 
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome(options=myoptions)
 source=driver.get('https://annapolislinux.org')
-source_code=driver.page_source
+html_source=driver.page_source
 
-soup = BeautifulSoup(source_code,'lxml')
+soup = BeautifulSoup(html_source,'lxml')
+
 #article_block = soup.findall('div',class_='post-title')
 
-print(soup.get_text())
+
+
+#condensed text
+print (soup.get_text("|", strip=True))
+
+#text with html
+#print (soup.prettify())
+
+
+#inspect each string
+#lines=(soup.get_text())
+#for string in lines:
+#  print(string)
+
+
+soup = BeautifulSoup(html_source, 'html.parser')
+for tag in soup.find_all('title'):
+    print(repr((tag.sourceline, tag.sourcepos, tag.string)))
