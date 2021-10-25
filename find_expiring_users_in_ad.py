6eb6a9888cf589430fb9a04ec7fecc72c10275d1
Theodore Knab <ipmasqman@gmail.com>
2021-10-25 15:23:28 -0400

    edit

--- find_expiring_users_in_ad.py
+++ find_expiring_users_in_ad.py
@@ -13,7 +13,7 @@ from botocore.exceptions import ClientError
 
 def get_secret():
 
-    secret_name = "arn:aws:secretsmanager:us-east-1:6636233238198:secret:test-ldapadmin-XHB1"
+    secret_name = "arn:aws:secretsmanager:us-east-1:6631234568:secret:test-ldapadmin-XHB1"
     region_name = "us-east-1"
 
     # Create a Secrets Manager client
