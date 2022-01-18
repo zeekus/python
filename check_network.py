4c3c3c6c85aeef26c257d0c849fe968cd51d68ee
Theodore Knab <ipmasqman@gmail.com>
2022-01-18 11:34:16 -0500

    added

--- check_network.py
+++ check_network.py
@@ -9,17 +9,25 @@ destination="google.com"
 
 def get_next_hop(destination):
   last_line=""
-  traceroute_cmd=("traceroute -m 4 %s" % destination) 
+  traceroute_cmd=("traceroute -m 4 %s| awk {'print $2'}" % destination) #get the ip
   proc = subprocess.Popen(traceroute_cmd, shell=True,stdout=subprocess.PIPE)
-  traceroute = proc.stdout.read()
+  traceroute = proc.stdout.readlines()
   for line in traceroute:
-      last_line=line
+      #print(line)
+      last_line=line.decode("utf-8").rstrip()
+
   return last_line
 
-#def ping_next_hop():
+def ping_last(ip):
+  ping_cmd=("ping -c 3 %s" % ip) #ping ip
+  proc = subprocess.Popen(ping_cmd, shell=True,stdout=subprocess.PIPE)
+  ping = proc.stdout.readlines()
+  for line in ping:
+      print(line)
 
-last_line=get_next_hop(destination)
-print (last_line)
+my_ip=get_next_hop(destination)
+print ("ip is '%s'" % my_ip)
+ping_last(my_ip)
 
 
 
