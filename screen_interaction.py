cf66d7b8f6498bfed3c659bc9e7af11a15cb83e5
Theodore Knab <ipmasqman@gmail.com>
2022-07-06 15:49:16 -0400

    added human like movement function

--- screen_interaction.py
+++ screen_interaction.py
@@ -2,14 +2,65 @@
 #filename: screen_interaction.py
 #description: basic screen interaction python.
 
+#from shutil import move
 import win32gui
 import win32api
 import win32con
 import inspect #look at objects
 import time
+import numpy as np
 #python3 -m pip install win32gui 
 #python3 -m pip install win-api #win32api 
 #python3 -m pip install pypiwin32 #win32con
+
+
+sqrt3 = np.sqrt(3)
+sqrt5 = np.sqrt(5)
+
+def wind_mouse(start_x, start_y, dest_x, dest_y, G_0=9, W_0=3, M_0=15, D_0=12, move_mouse=lambda x,y: None):
+    '''
+    WindMouse algorithm. Calls the move_mouse kwarg with each new step.
+    Released under the terms of the GPLv3 license.
+    G_0 - magnitude of the gravitational fornce
+    W_0 - magnitude of the wind force fluctuations
+    M_0 - maximum step size (velocity clip threshold)
+    D_0 - distance where wind behavior changes from random to damped
+    source: https://ben.land/post/2021/04/25/windmouse-human-mouse-movement/
+    '''
+    current_x,current_y = start_x,start_y
+    v_x = v_y = W_x = W_y = 0
+    while (dist:=np.hypot(dest_x-start_x,dest_y-start_y)) >= 1:
+        W_mag = min(W_0, dist)
+        if dist >= D_0:
+            W_x = W_x/sqrt3 + (2*np.random.random()-1)*W_mag/sqrt5
+            W_y = W_y/sqrt3 + (2*np.random.random()-1)*W_mag/sqrt5
+        else:
+            W_x /= sqrt3
+            W_y /= sqrt3
+            if M_0 < 3:
+                M_0 = np.random.random()*3 + 3
+            else:
+                M_0 /= sqrt5
+        v_x += W_x + G_0*(dest_x-start_x)/dist
+        v_y += W_y + G_0*(dest_y-start_y)/dist
+        v_mag = np.hypot(v_x, v_y)
+        if v_mag > M_0:
+            v_clip = M_0/2 + np.random.random()*M_0/2
+            v_x = (v_x/v_mag) * v_clip
+            v_y = (v_y/v_mag) * v_clip
+        start_x += v_x
+        start_y += v_y
+        move_x = int(np.round(start_x))
+        move_y = int(np.round(start_y))
+
+        print (move_x,move_y)
+        win32api.SetCursorPos((move_x,move_y))
+        time.sleep(0.01) #can we make this delay increase as time passes ? 
+        if current_x != move_x or current_y != move_y:
+            #This should wait for the mouse polling interval
+            #move_mouse(current_x:=move_x,current_y:=move_y)
+            win32api.SetCursorPos((move_x,move_y))
+    return current_x,current_y
 
 def get_screensize():
     w=win32api.GetSystemMetrics(0) #width
@@ -63,11 +114,12 @@ color=pixel_color(x,y)
 print ("pixel int color is '" + str(color) +"'")
 print ("pixel hex color is '" + hex(color) +"'")
 print("pixel r,g,b, truple color is " + str(rgbint2rgbtuple(color)) )
-x1=0
-y1=0
-print ("moving mouse to location " + str(x1) + "," + str(y1)) 
+dest_x=0
+dest_y=0
+print ("moving mouse to location " + str(dest_x) + "," + str(dest_y )) 
 
-left_click(x1,y1)
+wind_mouse(x, y, dest_x, dest_y, G_0=9, W_0=3, M_0=15, D_0=12, move_mouse=lambda dest_x,dest_y: None)
+left_click(dest_x,dest_y)
 
 # for name, obj in inspect.getmembers(win32api):
 #     print(str(name))
