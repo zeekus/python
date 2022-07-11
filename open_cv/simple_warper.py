7aa5b722c066276e57b80c216373d351f935d890
Theodore Knab <ipmasqman@gmail.com>
2022-07-11 08:37:50 -0400

    cleaned up code. new function.

--- open_cv/simple_warper.py
+++ open_cv/simple_warper.py
@@ -8,10 +8,10 @@ import os
 import pyautogui
 import json
 
-def  targets_files(path,json_file,target_message):
+def  load_target_data_from_json(path,json_file,target_message):
   f=open(json_file)        #open file
   data = json.load(f)      #load json data into mem
-  f.close                   #close file
+  f.close                  #close file
 
   file_array=[]
   #loop thorough the json data
@@ -24,75 +24,73 @@ def  targets_files(path,json_file,target_message):
 
   return file_array
 
-def find_image_on_screen(message):
+def find_target_image_on_screen(message):
     return pyautogui.locateOnScreen(message, confidence=0.9)
 
-def find_image_in_array(my_array):
+def find_one_image_from_many_files(my_array):
     for image in my_array:
-      result=find_image_on_screen(image)
+      result=find_target_image_on_screen(image)
       if result != None:
          return result
 
-path=os.getcwd()
-buttons_folder=(path + "/buttons/")
-button_json_file =(buttons_folder + "buttons.json")
-messages_folder=(path + "/messages/")
-message_json_file=(messages_folder+ "messages.json")
+def click_button(x,y,speed,description):
+    #click button at location
+    pyautogui.moveTo(x,x,speed, pyautogui.easeOutQuad)    # start fast, end slow
+    print("clicking " + description + " button at:" +  "x:" + str(x) + "y:" + str(y))
+    pyautogui.click(x, y)
+
+path=os.getcwd() #get current working directory 
+buttons_folder=(path + "/buttons/") #button images
+button_json_file =(buttons_folder + "buttons.json")  #description of button images
+messages_folder=(path + "/messages/") #message images
+message_json_file=(messages_folder+ "messages.json") #description of message images
 undock_image_exists = None #in space
 
 while undock_image_exists == None:
-    
-    print("checking for undock image.")
+    print("checking for undock images.")
     undock_buttons=[]
-    undock_buttons=targets_files(buttons_folder,button_json_file,"undock button found")
-    undock_image_exists=find_image_in_array(undock_buttons)
+    undock_buttons=load_target_data_from_json(buttons_folder,button_json_file,"undock button found")
+    undock_image_exists=find_one_image_from_many_files(undock_buttons)
 
-    #find nad click the yellow destination icon 
+    #find and click the yellow destination icon 
     yellow_result=None
 
     while yellow_result==None:
       print("checking for the yellow icon.")
       yellow_destination_icon=[]
-      yellow_destination_icon=targets_files(buttons_folder,button_json_file,"yellow gate icon")
-      yellow_result=find_image_in_array(yellow_destination_icon)
+      yellow_destination_icon=load_target_data_from_json(buttons_folder,button_json_file,"yellow gate icon")
+      yellow_result=find_one_image_from_many_files(yellow_destination_icon)
       time.sleep(2) #sleep for 2 seconds
 
     if yellow_result is not None :
       #click yellow icon
-      pyautogui.moveTo(yellow_result[0],yellow_result[1],2, pyautogui.easeOutQuad)    # start fast, end slow
-      print("clicking yellow icon at:" +  str(yellow_result))
-      pyautogui.click(x=yellow_result[0], y=yellow_result[1])
+      click_button(x=yellow_result[0],y=yellow_result[1],speed=.5,description="yellow icon")
 
       #verify the align button is visible
       align_button_found=None
       ready_to_align=[]
-      ready_to_align=targets_files(buttons_folder,button_json_file,"aligning button configuration")
-      align_button_found=find_image_in_array(ready_to_align)
+      ready_to_align=load_target_data_from_json(buttons_folder,button_json_file,"aligning button configuration")
+      align_button_found=find_one_image_from_many_files(ready_to_align)
 
       #press jump button if align button is on screen
-      if ready_to_align is not None:
+      if align_button_found is not None:
         clickable_jump_icon=None
         jump_buttons=[]
-        jump_buttons=targets_files(buttons_folder,button_json_file,"jump button")
-        clickable_jump_icon=find_image_in_array(jump_buttons)
-        if clickable_jump_icon!=None:
+        jump_buttons=load_target_data_from_json(buttons_folder,button_json_file,"jump button")
+        clickable_jump_icon=find_one_image_from_many_files(jump_buttons)
+        if clickable_jump_icon is not None:
           #click jump button 
-          pyautogui.moveTo(clickable_jump_icon[0],clickable_jump_icon[1],2, pyautogui.easeOutQuad)    # start fast, end slow
-          print("clicking jump button at:" +  str(clickable_jump_icon))
-          pyautogui.click(x=clickable_jump_icon[0], y=clickable_jump_icon[1])
-          
+          click_button(x=clickable_jump_icon[0],y=clickable_jump_icon[1],speed=.5,description="jump button")
+            
           #wait until jumping message appears
           jump_message_found=None
 
           while jump_message_found is None:
             jumping_messages=[]
-            jumping_messages=targets_files(messages_folder,message_json_file,"jumping")
-            jump_message_found=find_image_in_array(jumping_messages)
+            jumping_messages=load_target_data_from_json(messages_folder,message_json_file,"jumping")
+            jump_message_found=find_one_image_from_many_files(jumping_messages)
             time.sleep(.5)
-
           #if jump_message_found is not None:
           if jump_message_found is not None:
             print("Jumping Sequence detected.")
             time.sleep(5)
-
-
