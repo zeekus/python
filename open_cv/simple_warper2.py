94d8957485592ccd4636007e1b8226b55c7480ad
zeekus <ipmasqman@gmail.com>
2022-07-12 15:28:23 -0400

    added buttons

--- open_cv/simple_warper2.py
+++ open_cv/simple_warper2.py
@@ -15,7 +15,7 @@ def  load_target_data_from_json(path,json_file,target_message):
   f.close                  #close file
 
   file_array=[]
-  #loop thorough the json data
+  #loop through the json data
   for i in data:
     message=i['message']
     filename=i['filename']
@@ -29,10 +29,12 @@ def find_target_image_on_screen(message):
     return pyautogui.locateOnScreen(message, confidence=0.85)
 
 def find_one_image_from_many_files(my_array):
+    #print("find_one_image_from_many_files")
     for image in my_array:
       result=find_target_image_on_screen(image)
       if result != None:
          return result,image #location result and imagefile
+    return None,""
 
 def click_button(x,y,speed,description):
     #click button at location
@@ -41,13 +43,11 @@ def click_button(x,y,speed,description):
     pyautogui.click(x,y)
 
 def exit_if_docked(buttons_folder,button_json_file,mystart):
-    #print("checking for undock images.")
-    undock_buttons=[]
-    undock_buttons=load_target_data_from_json(buttons_folder,button_json_file,"undock button found")
-    undock_image_exists,imagefile=find_one_image_from_many_files(undock_buttons)
+    undock_image_exists,dfile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="undock button found")
+   
     if undock_image_exists != None: #in station
        print("we appear docked. Exiting.")
-       print("run time was " + str(time.time()-mystart) + " seconds")
+       print("We appear to be docked. Exiting. run time: " + str(time.time()-mystart) + " seconds")
        sys.exit()
     else:
       return None
@@ -60,7 +60,10 @@ def search_for_image_return_location(path,data_file,target):
    return result,imagefile
 
 def search_for_image_return_center_location(imagefile):
-   return center=locateCenterOnScreen(imagefile,confidence=0.9)
+   print("attempting to find center in " + str(imagefile) )
+   #return pyautogui.locateOnScreen(message, confidence=0.85)
+   return pyautogui.locateCenterOnScreen(imagefile,confidence=0.85)
+
 
 path=os.getcwd() #get current working directory 
 buttons_folder=(path + "/buttons/") #button images
@@ -80,46 +83,49 @@ while undock_image_exists == None:
       print(str(mytime) + " checking for the yellow icon.")
       yellow_destination_icon=[]
       yellow_destination_icon=load_target_data_from_json(buttons_folder,button_json_file,"yellow gate icon")
-      yellow_result,imagefile=find_one_image_from_many_files(yellow_destination_icon)
+      yellow_result,yfile=find_one_image_from_many_files(yellow_destination_icon)
+      print ("yellow results:" + str(yellow_result),str(yfile))
       time.sleep(2) #sleep for 2 seconds
 
     #verify the align button is visible
-    align_button_found,imagefile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="align_overview")
+    align_button_found,afile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="align overview")
     print("align button check result is " + str(align_button_found))
 
     if yellow_result is not None and align_button_found is None:
       #click yellow icon to get overview to refresh
-      print("clicking on yellow icon")
-      center=search_for_image_return_center_location(imagefile)
-      click_button(x=center[0],y=center[1],speed=1,description="yellow icon")
-      #click_button(x=yellow_result[0],y=yellow_result[1],speed=2,description="yellow icon")
+      print("clicking on yellow icon at " + yfile )
+      center=search_for_image_return_center_location(yfile)
+      print("center is " + str(center))
+      #click_button(x=center[0],y=center[1],speed=1,description="yellow icon")
+      click_button(x=yellow_result[0],y=yellow_result[1],speed=2,description="yellow icon")
       #check for align button 
-      align_button_found,imagefile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="align_overview")
+      align_button_found,afile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="align_overview")
       print("align button check result is " + str(align_button_found))
-    end
     
     if align_button_found is not None:
 
       #press jump button if align button is on screen
       if align_button_found is not None:
         clickable_jump_icon=None
-        clickable_jump_icon,imagefile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="jump button")
-       
+        clickable_jump_icon,jfile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="jump button")
         
         if clickable_jump_icon is not None:
           #click jump button 
-          center=search_for_image_return_center_location(imagefile)
+          center=search_for_image_return_center_location(jfile)
           #click_button(x=clickable_jump_icon[0],y=clickable_jump_icon[1],speed=2,description="jump button")
           click_button(x=center[0],y=center[1],speed=1,description="jump button")
+          jump_sequence_start=time.time()
+          time.sleep(5)
+          jump_message_found,jfile=search_for_image_return_location(path=messages_folder,data_file=message_json_file,target="jumping")
             
-          #wait until jumping message appears
-          jump_message_found=None
-          dock_image_found=exit_if_docked(buttons_folder,button_json_file,mystart)
 
-          while jump_message_found is None and dock_image_found is None:
-            jump_message_found,imagefile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="jummping")
-            dock_image_found=exit_if_docked(buttons_folder,button_json_file,mystart)
-            time.sleep(.5)
+          while jump_message_found is None:
+            #print("in jump sequence.")
+            jump_message_found,jfile=search_for_image_return_location(path=messages_folder,data_file=message_json_file,target="jumping")
+            if ( time.time()-jump_sequence_start > 60 ):
+               dock_image_found=exit_if_docked(buttons_folder,button_json_file,mystart) #look for docking image
+               print("Warning after " + str(time.time()-jump_sequence_start) + " seconds. We are still watitng for a jump message." )
+
           if jump_message_found is not None:
             print("Jumping Sequence detected.")
             time.sleep(5)
