139469886eb54f886a008a9736b24c268c7253f5
Theodore Knab <ipmasqman@gmail.com>
2022-07-15 08:24:46 -0400

    changed if statements to while

--- open_cv/simple_warper2.py
+++ open_cv/simple_warper2.py
@@ -1,7 +1,8 @@
 
 #!/usr/bin/python3
-#filename: simple_warper.py
-#uses pyautogui to control a space ship in warp. 
+#filename: simple_warper2.py
+#author: Theodore Knab
+#description: uses pyautogui to control a space ship in warp. 
 
 import time
 import os
@@ -65,7 +66,6 @@ def search_for_image_return_center_location(imagefile):
    #return pyautogui.locateOnScreen(message, confidence=0.85)
    return pyautogui.locateCenterOnScreen(imagefile,confidence=0.85)
 
-
 path=os.getcwd() #get current working directory 
 buttons_folder=(path + "/buttons/") #button images
 button_json_file =(buttons_folder + "buttons.json")  #description of button images
@@ -73,7 +73,6 @@ messages_folder=(path + "/messages/") #message images
 message_json_file=(messages_folder+ "messages.json") #description of message images
 mystart=time.time()
 undock_image_exists = exit_if_docked(buttons_folder,button_json_file,mystart)
-
 
 while undock_image_exists == None:
     #find and click the yellow destination icon 
@@ -88,47 +87,56 @@ while undock_image_exists == None:
 
     #verify the align button is visible
     align_button_found,afile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="align overview")
-    print("align button check result is " + str(align_button_found))
+    print("overview check for align_button image returns " + str(align_button_found))
 
     no_obj_found,no_o_file=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="no object selected")
-    print("no object check result is " + str(no_obj_found))
+    print("overview check for no object image returns " + str(no_obj_found))
     
-
-    if (( yellow_result is not None and no_obj_found) or align_button_found is None):
+    #click on yellow icon 
+    while (( yellow_result is not None and no_obj_found is None) or align_button_found is None):
       #click yellow icon to get overview to refresh
-      print("clicking on yellow icon at " + yfile )
+      print("clicking on yellow icon to refresh overview " + yfile )
       center=search_for_image_return_center_location(yfile)
-      print("center is " + str(center))
-      #click_button(x=center[0],y=center[1],speed=1,description="yellow icon")
-      click_button(x=yellow_result[0],y=yellow_result[1],speed=2,description="yellow icon")
-      #check for align button 
-      align_button_found,afile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="align_overview")
-      print("align button check result is " + str(align_button_found))
+      
+      if center is not None:
+        print("... yellow icon center is " + str(center))
+        click_button(x=center[0],y=center[1],speed=1,description="yellow icon") #yellow center
+      else:
+        click_button(x=yellow_result[0],y=yellow_result[1],speed=2,description="yellow icon")
+
+      #verify the align button is visible
+      align_button_found,afile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="align overview")
+      print("overview check for align_button image returns " + str(align_button_found))
+
+      time.sleep(3)
     
-    if align_button_found is not None:
+    #click on jump button if align button exists
+    while align_button_found is not None:
 
       #press jump button if align button is on screen
-      if align_button_found is not None:
-        clickable_jump_icon=None
-        clickable_jump_icon,jfile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="jump button")
+      clickable_jump_icon=None
+      clickable_jump_icon,jfile=search_for_image_return_location(path=buttons_folder,data_file=button_json_file,target="jump button")
         
-        if clickable_jump_icon is not None:
-          #click jump button 
-          center=search_for_image_return_center_location(jfile)
-          #click_button(x=clickable_jump_icon[0],y=clickable_jump_icon[1],speed=2,description="jump button")
+      if clickable_jump_icon is not None:
+        #click jump button 
+        center=search_for_image_return_center_location(jfile)
+        if center is not None:
           click_button(x=center[0],y=center[1],speed=1,description="jump button")
-          jump_sequence_start=time.time()
-          time.sleep(5)
-          jump_message_found,jfile=search_for_image_return_location(path=messages_folder,data_file=message_json_file,target="jumping")
-            
+        else:
+          click_button(x=clickable_jump_icon[0],y=clickable_jump_icon[1],speed=2,description="jump button")
+          
+        jump_sequence_start=time.time()
+        time.sleep(5)
+        jump_message_found,jfile=search_for_image_return_location(path=messages_folder,data_file=message_json_file,target="jumping")
 
-          while jump_message_found is None:
-            #print("in jump sequence.")
-            jump_message_found,jfile=search_for_image_return_location(path=messages_folder,data_file=message_json_file,target="jumping")
-            if ( time.time()-jump_sequence_start > 60 ):
-               dock_image_found=exit_if_docked(buttons_folder,button_json_file,mystart) #look for docking image
-               print("Warning after " + str(time.time()-jump_sequence_start) + " seconds. We are still watitng for a jump message." )
+    #in jump sequence. waiting for end - "Jumping" message. - Note only checking one visual variable may create a bug.
+    while jump_message_found is None:
+      jump_message_found,jfile=search_for_image_return_location(path=messages_folder,data_file=message_json_file,target="jumping")
+      if ( time.time()-jump_sequence_start > 60 ):
+        dock_image_found=exit_if_docked(buttons_folder,button_json_file,mystart) #look for docking image
+        print("Warning after " + str(time.time()-jump_sequence_start) + " seconds. We are still watitng for a jump message." )
 
-          if jump_message_found is not None:
-            print("Jumping Sequence detected.")
-            time.sleep(5)
+    if jump_message_found is not None:
+      jump_seq_time=time.time()-jump_sequence_start
+      print("Jumping Sequence detected. sequence ran:" + jump_seq_time + " seconds" )
+      time.sleep(5)
