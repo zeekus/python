7ba17c5fe6e8b2240f23d098f4016acc49d22e12
Theodore Knab <ipmasqman@gmail.com>
2022-08-29 10:25:22 -0400

    updated code

--- count_characters.py
+++ count_characters.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python
 #filename: count_characters.py
 #date: 08 Sept 2019 
-#descriptiion: a python program that counts charactes in a string
+#updated 8/29/22 to comply with python3 format
+#description: a python program that counts characters in a string
 
 import sys
-print "This is the name of the script: ", sys.argv[0]
+print ("This is the name of the script: ", sys.argv[0])
 
-print "Number of arguments: ", len(sys.argv)
+print ("Number of arguments: ", len(sys.argv))
 
 if ( len(sys.argv)  == 1 ):
-  print "No data found exiting..."
-  print "You need to type a line of characters. Try a name.\n .. use example: "  + sys.argv[0] + " 'Matt Krist'"
+  print ("No data found exiting...")
+  print ("You need to type a line of characters. Try a name.\n .. use example: "  + sys.argv[0] + " 'Matt Smith'")
   sys.exit(0)
 else:
-  print "The arguement you provides was: ", sys.argv[1]
-  print "...counting characters\n"
+  print ("The arguement you provides was: ", sys.argv[1])
+  print ("...counting characters\n")
 
 string = sys.argv[1]
 count = 0
 for c in string:
   count = count + 1
 
-print "Characters counted : " + str(count) + " in " + "'" + string + "'"
+print ("Characters counted : " + str(count) + " in " + "'" + string + "'")
