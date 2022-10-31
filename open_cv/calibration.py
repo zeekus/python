5f5a5525c470ff864c2ad0514f62c35053c294b4
Theodore Knab <ipmasqman@gmail.com>
2022-10-31 15:26:24 -0400

    added calibration as a class

--- open_cv/calibration.py
+++ open_cv/calibration.py
@@ -5,38 +5,50 @@ import os,pyautogui
 
 class Calibration:
   #variables
-  screen_left_t =[0,0] #top left of screen
-  screen_rgt_b  =[0,0] #bottom right of screen
-  nav_bar_t     =[0,0] #top of nav bar
-  nav_bar_b     =[0,0] #bottom of nav bar
-  a_center      =[0,0] #align/approach center
-  jt_center     =[0,0] #jump/to center
-  jd_center     =[0,0] #jump/dock button center
-  i_icon        =[0,0,0,0] #icon start x,y dims
-  screen_center =[0,0]
+  # screen_left_t =[0,0] #top left of screen
+  # screen_rgt_b  =[0,0] #bottom right of screen
+  # nav_bar_t     =[0,0] #top of nav bar
+  # nav_bar_b     =[0,0] #bottom of nav bar
+  # a_center      =[0,0] #align/approach center
+  # jt_center     =[0,0] #jump/to center
+  # jd_center     =[0,0] #jump/dock button center
+  # i_icon        =[0,0,0,0] #icon start x,y dims
+  # screen_center =[0,0]
 
   def __init__(self, w=0,h=0):
     self.w = w # 1920 with one 3840 for two screens
     self.h = h # 1080 
     if w > 2000:
-      self.screen_left_t =[int(self.w/2),0] #top left of screen
+      self.top_left =[int(self.w/2)+3,3] #top left of screen
       self.screen_center=[int(self.w/4),int(self.h/2)] #get x with two screens
     else:
-      self.screen_left_t =[0,0] #top left of screen
+      self.top_left =[1,1] #top left of screen
       self.screen_center=[int(self.w/2),int(self.h/2)] #get x with two screens
 
-    self.screen_rgt_b=[self.w,self.h]
+    self.bottom_right=[self.w-3,self.h-3] #-3 on edges
     self.path=os.getcwd() #get current working directory 
     self.buttons_folder =self.path + "/buttons/"
     self.message_folder =self.path + "/message/"
     self.button_json_file =(self.buttons_folder + "buttons.json")  #description of button images
     self.message_json_file=(self.message_folder + "messages.json") #description of message images
-
+    self.navbar_ltop =[self.bottom_right[0]-410,30]
+    self.navbar_rbot =[self.bottom_right[0]-10,150] 
+    
   def display_variables(self):
-    print(self)
+    print(f"path is {self.path}")
+    print(f"moving mouse to 'top_left' at {self.top_left}")
+    pyautogui.moveTo(self.top_left[0],self.top_left[1],2)
+    print(f"moving mouse to 'bottom_right' at {self.bottom_right}")
+    pyautogui.moveTo(self.bottom_right[0],self.bottom_right[1],2)
+    print(f"moving mouse to 'screen_center' at {self.screen_center}")
+    pyautogui.moveTo(self.screen_center[0],self.screen_center[1],1)  
+    print(f"moving mouse to 'navbar_rtop' at {self.navbar_ltop}")
+    pyautogui.moveTo(self.navbar_ltop[0],self.navbar_ltop[1],1)
+    print(f"moving mouse to 'navbar_rbot' at {self.navbar_rbot}")
+    pyautogui.moveTo(self.navbar_rbot[0],self.navbar_rbot[1],1)
 
-my_screensize=pyautogui.size()
-w=my_screensize[0] #width  aka x
-h=my_screensize[1] #height aka y
-a=calibration(w,h)
-a.display_variables
+
+w,h=pyautogui.size()
+a=Calibration(w,h)
+a.display_variables()
+
