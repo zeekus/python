2f041a0fa3e558c67db32b17a043f6969ac11baf
Theodore Knab <tknab@chesapeakebay.net>
2023-06-05 11:49:38 -0400

    updated argv example

--- examples/argv.py
+++ examples/argv.py
@@ -5,5 +5,10 @@
 import sys
 
 print("This is the name of the program:", sys.argv[0])
+print("Raw - Argument List:", str(sys.argv))
 
-print("Argument List:", str(sys.argv))
+if len(sys.argv) == 1:
+    print("Error: No arguments found.")
+else:
+    print("Number of arguments:", len(sys.argv) - 1)
+    print("Argument List:", str(sys.argv[1:]))
