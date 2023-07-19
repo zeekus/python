98763cdeb89b4fb78df7e303cc59172b4314fddc
Theodore Knab <tknab@chesapeakebay.net>
2023-07-19 12:00:05 -0400

    adding debug statements

--- raspberrypi/night_time_nanny.py
+++ raspberrypi/night_time_nanny.py
@@ -158,32 +158,26 @@ def log_event(filestring,text):
 
 #sayit sends computer voice through the speakers
 def sayit(phrase):
-    cmd_echo = f'echo "{phrase}"'
+    if os.path.exists("/usr/bin/festival"):
+        cmd_talk = "festival --tts"
+    elif os.path.exists("/usr/bin/espeak"):
+        cmd_talk = "espeak -a 500 -p 1"
+    else:
+        text=("Sorry, this program will not work without Festival or espeak installed. Please install one.\n")
+        log_event("raspberrypi", text)
+        return
+
+    cmd_echo = f'echo "{phrase}" | {cmd_talk}'
 
     text=f"debug: sayit: {cmd_echo}"
     log_event("raspberrypi", text) 
 
-    if os.path.exists("/usr/bin/festival"):
-        cmd_talk = "festival --tts"
-        #errors get chatty so I am sending them to devnull
-        try:
-            status = subprocess.call(cmd_echo + "|" + cmd_talk, stderr=subprocess.DEVNULL, shell=True)
-        except Exception as e:
-            traceback.print_exc()
-            print(f"Error executing festival command: {e}")
-    elif os.path.exists("/usr/bin/espeak"):
-        cmd_talk = "espeak -a 500 -p 1 "
-        try:
-            status = subprocess.call(cmd_echo + "|" + cmd_talk, stderr=subprocess.DEVNULL, shell=True)
-        except Exception as e:
-            traceback.print_exc()
-            print(f"Error executing espeak command: {e}")
-    else:
-        print("Sorry, this program will not work without Festival or espeak installed. Please install one.\n")
-        sys.exit(1)
+    try:
+        status = subprocess.call(cmd_echo, stderr=subprocess.DEVNULL, shell=True)
+    except Exception as e:
+        traceback.print_exc()
 
         
-
 
 #############
 #MAIN area
