d99d5c7621029df5179da1d32d908aea8d5ab571
Theodore Knab <tknab@chesapeakebay.net>
2023-07-19 13:06:38 -0400

    updated cronjob

--- raspberrypi/monitor_night_time_nanny.bash
+++ raspberrypi/monitor_night_time_nanny.bash
@@ -5,16 +5,12 @@
 
 monitor_log="$(date +"%Y-%m-%d-monitor.log")"
 mydate=$(date)
+program="night_time_nanny.py"
 if ! pgrep -f "python night_time_nanny.py" > /dev/null; then
-    echo "night_time_nanny.py is not running. Starting it..."
-    if [ -f /home/ted/night_time_nanny.lock ]; then
-      echo "$mydate found lock file removing" >> $monitor_log
-      rm /home/ted/night_time_nanny.lock
-    fi
+    echo "$program is not running. Starting it..."
     mydate=$(date)
-    echo "$mydate starting nanny_cron_wrapper.py" >> $monitor_log
-    #starting wrapper program
-    python3 nanny_cron_wrapper.py & 
+    echo "$mydate starting $program" >> $monitor_log
+    python3 $program
 else
-    echo "$mydate nanny_cron_wrapper.py is running" >> $monitor_log
+    echo "$mydate $program is running" >> $monitor_log
 fi
