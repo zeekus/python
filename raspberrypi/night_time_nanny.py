fb809a182de2e48df5e10a61d1f1b628ac380c01
zeekus <ipmasqman@gmail.com>
2023-07-23 09:19:25 -0400

    debug2

--- raspberrypi/night_time_nanny.py
+++ raspberrypi/night_time_nanny.py
@@ -170,7 +170,7 @@ def log_event(filestring,text):
 
 #sayit sends computer voice through the speakers
 #to get this to work I had to convert the audio to a wave and send it using aplay 
-def sayit(text,volume,pitch,word_speed_per_minute):
+def sayit(text,volume,pitch,word_speed_per_minute,debug=0):
 
   if os.path.exists("/usr/bin/espeak"):
     # Rename the existing output file if it exists
@@ -181,12 +181,18 @@ def sayit(text,volume,pitch,word_speed_per_minute):
       os.remove(file)
 
     # Call the espeak command and redirect the output to a file
-    #espeak ref: -a 100 max volume
-    subprocess.call(["espeak", "-w", "output.wav", "-ven-us-nyc+f2","-z",str(volume),str(pitch),str(word_speed_per_minute),text])
+    if debug==1:
+      #generate sound clip
+      subprocess.call(["espeak", "-w", "output.wav", "-ven-us-nyc+f2","-z","-v",str(volume),str(pitch),str(word_speed_per_minute),text])
+      # Convert the mono WAV file to a stereo WAV file
+      subprocess.call(["sox", "output.wav", "-c", "2", "-V", "output_stereo.wav","tempo","1.00"])
+    else: 
+      #generate sound clip 
+      subprocess.call(["espeak", "-w", "output.wav", "-ven-us-nyc+f2","-z",str(volume),str(pitch),str(word_speed_per_minute),text])
+      # Convert the mono WAV file to a stereo WAV file
+      subprocess.call(["sox", "output.wav", "-c", "2","output_stereo.wav","tempo","1.00"])
 
-    # Convert the mono WAV file to a stereo WAV file
-    subprocess.call(["sox", "output.wav", "-c", "2", "-V", "output_stereo.wav","tempo","1.00"])
-
+    #play wave file in headless mode
     subprocess.call(["aplay","-Dplug:default","output_stereo.wav"])
     sayit=f"sayit - espeak: {text}"
     log_event("raspberrypi", sayit) 
