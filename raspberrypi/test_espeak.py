5e5efeca98a4b325711a3ec8f570ca1b70934544
zeekus <ipmasqman@gmail.com>
2023-08-10 07:20:04 -0400

    created test script

--- raspberrypi/test_espeak.py
+++ raspberrypi/test_espeak.py
@@ -11,7 +11,7 @@ def sayit( text, volume):
 
             for file in wav_files:
                 os.remove(file)
-            subprocess.call(["espeak", "-w", "output.wav", "-ven-us-nyc+f4", "-z", "-a", volume, "-p 10", "-s150", "-g 5", "-k 5", "-m",  text])
+            subprocess.call(["espeak", "-w", "output.wav", "-ven-gb+m1", "-z", "-a", volume, "-p", "1", "-s150", "-g", "5", "-k", "5", "-m", text])
             subprocess.call(["sox", "output.wav", "-c", "2", "output_stereo.wav"])
             subprocess.call(["aplay", "-Dplug:default", "output_stereo.wav"])
 
@@ -20,7 +20,7 @@ def sayit( text, volume):
             print("error missing espeak. exiting with 1")
             sys.exit(1)
 
-        time.sleep(15) #only talk every 15 seconds. 
+        time.sleep(5) #only talk every 15 seconds. 
 
 sayit_text = f"Hey!, close the door!"
 sayit(str(sayit_text), volume="200")
