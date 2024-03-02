f0cbfc6afdbeaae1d662f6fbc489de2660f0723f
Zeekus <ipmasqman@gmail.com>
2024-03-02 13:45:42 -0500

    updated test scripts - syntax fix

--- press_mouse_button.py
+++ press_mouse_button.py
@@ -12,23 +12,21 @@ def press_and_hold_left_mouse_button()
   pyautogui.mouseUp(button='left')
   
 # Loop for 10 seconds
+print("waiting for 10 seconds...")
+print("open the game you want to test.")
 for i in range(10):
     print(".", end="", flush=True)  # Print a dot without a newline and flush the output buffer
     time.sleep(1)  # Wait for 1 second
-
 print()  # Print a newline after the loop
 
-for i in range(600):
-  # Press the 'E' key
-  press_and_hold_left_mouse_button()
-  sys.exit()
+try: 
+  while True
+    press_and_hold_left_mouse_button()
+    # print("press e")
+    # pyautogui.keyDown('e')
+    # random_delay()
+    # pyautogui.keyUp('e') #release
+except KeyboardInterrupt:
+    print("Program interrupted by Ctrl-c")
 
-  print("press e")
-  pyautogui.keyDown('e')
-  delay=round(random.random() * 9 + 1,2)
-  print("waiting: " + delay)
-  time.sleep(delay)
-  if i % 10 ==0 :
-    # Release the 'W' key
-    pyautogui.keyUp('e')
   
