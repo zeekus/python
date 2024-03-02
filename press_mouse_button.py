fe0d82e4a9afe8c61d3c206dc96863fa648fb184
Zeekus <ipmasqman@gmail.com>
2024-03-02 13:54:23 -0500

    updated test script

--- press_mouse_button.py
+++ press_mouse_button.py
@@ -3,13 +3,19 @@ import time
 import random
 import sys
 
+
+
 def random_delay():
   time.sleep( round(random.random() * 9 + 1, 2))
 
-def press_and_hold_left_mouse_button()
+def press_and_hold_left_mouse_button(start_time):
+  print("pressing and holding mouse button")
   pyautogui.mouseDown(button='left')
   random_delay()
   pyautogui.mouseUp(button='left')
+  end_time=time.time()
+  elasped_time=end_time-start_time
+  print("releasing mouse button ran for " + str(elasped_time) + " secs")
   
 # Loop for 10 seconds
 print("waiting for 10 seconds...")
@@ -19,9 +25,13 @@ for i in range(10):
     time.sleep(1)  # Wait for 1 second
 print()  # Print a newline after the loop
 
+
+
 try: 
-  while True
-    press_and_hold_left_mouse_button()
+  while True:
+    start_time=time.time()
+    press_and_hold_left_mouse_button(start_time)
+
     # print("press e")
     # pyautogui.keyDown('e')
     # random_delay()
