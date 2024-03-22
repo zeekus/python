19864be602b24616545f74e37d6db7a059cf549f
Theodore Knab <tknab@chesapeakebay.net>
2024-03-22 11:23:45 -0400

    updated logic

--- check_cifs_read_write.py
+++ check_cifs_read_write.py
@@ -1,36 +1,35 @@
 #!/bin/python
 #filename: check_cifs_read_write.py
 #description: checks the cifs and sends an email if there is an issue.
+#checked more to work around cifs timeouts
 
 import sys
 import os
 import re
 import subprocess
 import datetime
+import time
 
 # Generate a timestamp in the format YYYYMMDD_HHMMSS
 timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
 # Update the file name with the timestamp
 file_name = "cifstests_{}.txt".format(timestamp)
-script_name = os.path.basename(__file__) #name of this file. 
+script_name = os.path.basename(__file__) #name of this file.
 
 # Global variables
 error_count = 0
-email_recipient = 'myuser@example.net'
+max_error_count = 2  # Maximum number of allowed errors
+email_recipient = 'mymail@example.net'
 email_subject = "CIFS Write Failure"
-data = [""] #data for body of the email 
-SERVERNAME=os.uname().nodename if hasattr(os, 'uname') else os.getenv('HOSTNAME', os.getenv('COMPUTERNAME')) #get hostname of server 
+data = [""] #data for body of the email
+SERVERNAME=os.uname().nodename if hasattr(os, 'uname') else os.getenv('HOSTNAME', os.getenv('COMPUTERNAME')) #get hostname of server
 imagery_file_path = "/media/imagery/{}".format(file_name)
 media_file_path = "/media/share/GIS/{}".format(file_name)
 
-#def read_lines_from_file(file_path):
-#    """Read lines from a file and return them as a list."""
-#    with open(file_path) as file:
-#        lines = file.readlines()
-#    return lines
-
 def write_lines_to_file(file_path, lines):
+    os.system('sync')
+    time.sleep(2)
     """Write lines to a file."""
     with open(file_path, mode="w") as file:
         for line in lines:
@@ -47,6 +46,8 @@ def write_file_with_error_handling(file_path, lines):
         error_count += 1
 
 def remove_file_with_error_handling(file_path):
+    os.system('sync')
+    time.sleep(2)
     """Remove a file with error handling."""
     try:
         os.remove(file_path)
@@ -55,22 +56,22 @@ def remove_file_with_error_handling(file_path):
         print("Error removing file: {}".format(file_path))
         global error_count
         error_count += 1
-		
-print ("debug info on {}.".format(script_name))
+
+print("Debug info on {}.".format(script_name))
 print(imagery_file_path)
 print(media_file_path)
 
 write_file_with_error_handling(imagery_file_path, data)
 write_file_with_error_handling(media_file_path, data)
 
-if error_count > 0:
+if error_count >= max_error_count:
     df_output = subprocess.check_output(["df", "-h"]).decode('utf-8')
-    message = "Errors occurred during CIFS file writes on {}. This may indicate an unmounted filesystem.\nError count: {}\n{}".format(SERVERNAME, error_count, df_output)
+    message = "Errors occurred during CIFS file writes on {}. This may indicate an unmounted filesystem.\nError count: {}/{}\n{}".format(SERVERNAME, error_count, max_error_count, df_output)
     print(message)
     os.system("echo '{}' | mutt -s '{}' {}".format(message, email_subject, email_recipient))
 else:
     print("Success. No errors occurred during the writes on {}.".format(SERVERNAME))
 
-
 remove_file_with_error_handling(imagery_file_path)
 remove_file_with_error_handling(media_file_path)
+
