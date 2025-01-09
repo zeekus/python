4dfd62f384a573aa8ad99de508d93c41c909e1e2
Teddy Knab <31280061+zeekus@users.noreply.github.com>
2025-01-09 07:59:55 -0500

    Update Trade.py
    logic update

--- trader/proto2/Trade.py
+++ trader/proto2/Trade.py
@@ -1,23 +1,23 @@
-
 # filename: Trade.py
 # Description: Processes Trades.
 # Copyright (C) 2025 Theodore Knab
 # Special thanks to Michael von den Driesch who provided the FIFO logic
 # Also Special thanks to ChatGPT
+
 class Trade:
     def __init__(self, date, crypto_asset, crypto_amount, total_amt, crypto_fee, usd_fee, crypto_balance, txid, refid):
         self.date = date
         self.crypto_asset = crypto_asset
         self.crypto_amount = crypto_amount
-        self.total_amt = total_amt  # Renamed from usd_amount to total_amt
+        self.total_amt = total_amt
         self.crypto_fee = crypto_fee
         self.usd_fee = usd_fee
         self.txid = txid
-        self.refid = refid  # Added refid for querying FIAT wallet
+        self.refid = refid
         self.is_buy = crypto_amount > 0
         self.actual_crypto_amount = crypto_amount - crypto_fee if self.is_buy else crypto_amount
         self.crypto_balance = self.actual_crypto_amount if self.is_buy else crypto_balance
-        self.fiat_equivalent = None  # To be set later by querying FIAT wallet
+        self.fiat_equivalent = None
 
     def set_fiat_equivalent(self, fiat_amount):
         self.fiat_equivalent = fiat_amount
