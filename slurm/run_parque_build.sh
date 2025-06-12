da45f6e75a9af6bde0dbb2645e30cf3b61178f56
zeekus <ipmasqman@gmail.com>
2025-06-12 10:59:32 -0400

    updated

--- slurm/run_parque_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/u/tknab/mypythonenv
-source venv/bin/activate
-bucket="mybucketname"
-folder="myfoldername"
-cost="1.581"
-sudo cat /var/log/slurmctld.log | python3 $HOME/cronjobs/parquet_log_building1mo.py -C $cost --s3-bucket $bucket/$folder
-deactivate
